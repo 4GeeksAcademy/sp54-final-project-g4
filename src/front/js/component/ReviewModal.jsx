609e95d94cbb87bed1186e9a017195c08f7329b8
Alfredo <146830711+JockerAG@users.noreply.github.com>
2024-03-29 18:11:24 +0000

    feat:Modal reviews terminado
    Falta funcionalidades js

--- src/front/js/component/ReviewModal.jsx
+++ src/front/js/component/ReviewModal.jsx
@@ -1,33 +1,38 @@
-import React, { useState } from "react";
+import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 
 
-
-export const ModalForReview = ({ onHide }) => {
-   
+function ReviewModal(props) {
     return (
-        <>
-            <Modal show={true} onHide={onHide} dialogClassName="modal-200w" aria-labelledby="example-custom-modal-styling-title" >
-                <Modal.Header closeButton>
-                    <Modal.Title className="text-center">
-                        <i class="fa-regular fa-star fa-2x"></i>
-                        <i class="fa-regular fa-star fa-2x"></i>
-                        <i class="fa-regular fa-star fa-2x"></i>
-                        <i class="fa-regular fa-star fa-2x"></i>
-                        <i class="fa-regular fa-star fa-2x"></i>
-                    </Modal.Title>
-                    
-                </Modal.Header>
-
-                <Modal.Body>
-                    <h4 className="text-center">Review</h4>
-                    <textarea id="reviewTextArea" className="form-control" style={{ width: '100%', height: '200px' }} placeholder="Add your review">
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    <h1 className='text-center fs-5'>Add your Review</h1>
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <h4 className='text-center'>
+                    <i className="fa-regular fa-star fa-2x text-warning text-opacity-50"></i>
+                    <i className="fa-regular fa-star fa-2x text-warning text-opacity-50"></i>
+                    <i className="fa-regular fa-star fa-2x text-warning text-opacity-50"></i>
+                    <i className="fa-regular fa-star fa-2x text-warning text-opacity-50"></i>
+                    <i className="fa-regular fa-star fa-2x text-warning text-opacity-50"></i>
+                </h4>
+            </Modal.Body>
+            <Modal.Body>
+            <textarea id="reviewTextArea" className="form-control" style={{ width: '100%', height: '200px' }} placeholder="Add your review">
                     </textarea>
-                </Modal.Body>
-                <Modal.Footer>
-                    <button className="btn btn-success btn-sm">Add review</button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    )
+            </Modal.Body>
+            <Modal.Footer>
+                <button className="btn btn-success btn-sm">Add review</button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
+
+export default ReviewModal
