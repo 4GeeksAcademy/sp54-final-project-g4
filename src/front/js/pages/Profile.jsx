5e2d840cdca8e76302e7665ef7bb6f6fc9bad4f9
Alfredo <146830711+JockerAG@users.noreply.github.com>
2024-04-04 19:16:01 +0000

    wip:Profile

--- src/front/js/pages/Profile.jsx
+++ src/front/js/pages/Profile.jsx
@@ -1,23 +1,56 @@
-import React from "react";
-import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
+import React, { useContext, useEffect, useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+import { Image, Button } from 'react-bootstrap'
+
 
 export const Profile = () => {
+    const { actions, store } = useContext(Context);
+    // useStates
+    const [currentUser, setCurrentUser] = useState(null)
+    const [infoProfile, setInfoProfile] = useState(null)
+    const [isSameUser, setIsSameUser] = useState(false)
+    const params = useParams(); // esto es igual a : es lo que va dentro de Params 
+    const getCurrentUser = async () => {
+        if (localStorage.getItem('access_token')) {
+            setCurrentUser(await actions.getUserLoggedIn())
+        } 
+
+    }
+
+
+    const getProfile = async () => {
+        setInfoProfile(await actions.getUser(params.username))
+    }
+
+    const checkIsSameUser = () => {
+        if(currentUser.results.username.toLowerCase() == params.username.toLowerCase()){
+            setIsSameUser(true)
+        }
+    }
+
+    useEffect(() => {
+        getCurrentUser()
+        getProfile()
+        checkIsSameUser()
+    }, [])
+
+
 
     return (
         <div className="container-fluid d-flex justify-content-inline m-3">
             <div >
                 <Image src="https://placehold.co/100x100" roundedCircle className="mb-3" style={{ 'max-width': '16rem', 'min-width': '16rem' }} />
-                <div class="card text-bg-secondary mt-3" style={{ 'max-width': '16rem' }}>
-                    <Button variant="secondary" className="d-grid gap-2">Edit Profile</Button>
-                    <div class="card-body bg-light mt-2">
-                        This is some text within a card body.
+                <div className="card text-bg-secondary mt-3" style={{ 'max-width': '16rem' }}>
+                    <Button variant="secondary" className={isSameUser ? "d-grid gap-2" : "d-grid gap-2 disabled"}>Edit Profile</Button>
+                    <div className="card-body bg-light mt-2 text-dark">
+                        {infoProfile ? infoProfile.results.username : "" }
                     </div>
                 </div>
             </div>
             <div className="container d-flex justify-content-between mx-3">
                 <div >
-                    <h1 className="mt-3">Nombre Usuario</h1>
+                    <h1 className="mt-3">{params.username}</h1>
                     <p>0 creditos</p>
                     <div className="d-flex justify-content-inline mt-5">
                         <h4 className="me-3">0 Following</h4>
