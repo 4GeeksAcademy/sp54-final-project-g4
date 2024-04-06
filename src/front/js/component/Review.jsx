2796835f4dccc4fc50c61f98ef58b0bec3731184
Annie Roldán Rodríguez <149087336+AnnieRoro@users.noreply.github.com>
2024-04-06 09:38:07 +0000

    fat:reviews
    faltaba el boton de comentarios y eliminar

--- src/front/js/component/Review.jsx
+++ src/front/js/component/Review.jsx
@@ -8,14 +8,20 @@ export const Review = () => {
         <div >
 
             <Card className="col-lg-6 col-sm-3 col-md-6 bg-light m-3">
-                <Card.Header><b>El Peluche </b> <i className="fas fa-star" style={{"color": "#FFD43B"}}></i>2/5</Card.Header>
+                <Card.Header className="d-flex justify-content-between">
+                    <p><b>El Peluche </b> <i className="fas fa-star" style={{"color": "#FFD43B"}}></i>2/5</p>
+                    <i className="fas fa-times text-secondary"></i>
+                </Card.Header>
                 <Card.Body>
                     <Card.Title>Muy mala</Card.Title>
                     <Card.Text>
                         No me ha gustado la peli.
                     </Card.Text>
                 </Card.Body>
-                <Card.Footer className="text-muted">2 days ago</Card.Footer>
+                <Card.Footer className="text-muted d-flex justify-content-between">
+                    <p>2 days ago</p>
+                    <i className="fas fa-comments"></i>
+                </Card.Footer>
             </Card>
         </div>
 
